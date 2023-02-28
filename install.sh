540a7043cc6b65a540d3b122a16e936195fb1cf3
Maxim Savenkov <max_savenkov@mail.ru>
2023-02-28 15:12:55 +0100

    Install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 rm -rf target
 mvn install
-rm ~/.m2/repository/dev/mv/mvengine/1.0/mvengine-1.0.pom
-cp installed.xml ~/.m2/repository/dev/mv/mvengine/1.0/mvengine-1.0.pom
+rm ~/.m2/repository/dev/mv/engine/1.0/engine-1.0.pom
+cp installed.xml ~/.m2/repository/dev/mv/engine/1.0/engine-1.0.pom
